5224fff4ee14217c52b4fab9fdf3479a9a47ff7a
Josh S <aaabccc87@protonmail.com>
2022-07-29 20:27:57 -0400

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+  },
+  optimization: {
+    minize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /template\.html$/,
+        type: 'asset/source',
+      },
+    ],
+  },
+};
